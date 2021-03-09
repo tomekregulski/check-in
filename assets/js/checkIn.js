55602d3bd71150608f37f7e8ef0becbba28af825
Tomek <tomek.regulski@gmail.com>
2021-03-09 08:33:16 -0500

    adjusted button positioning

--- assets/js/checkIn.js
+++ assets/js/checkIn.js
@@ -1,25 +1,17 @@
-// Initialize and add the map
-var marker;
 var cityName = document.getElementById('cityName');
-var temperature = document.getElementById('temperature');
-var humidity = document.getElementById('humidity');
-var windSpeed = document.getElementById('windSpeed');
-var uvIndex = document.getElementById('uvIndex');
 var savedCities = document.getElementById('savedCities');
-var btnClass;
-var iconDailyDiv;
-var iconDailyAppend;
 var savedCitiesArray = JSON.parse(localStorage.getItem("savedCities")) || [];
-var weatherBtn = document.getElementById('weatherBtn');
+var checkInBtn = document.getElementById('checkInBtn');
 
 
 function getLocation() {
     window.navigator.geolocation.getCurrentPosition((location) => {
           console.log(location);
-          alert(`your coordinates are ${location.coords.latitude}, ${location.coords.longitude}`);
           console.log(location.timestamp);
           var timestamp = location.timestamp;
           console.log(moment(timestamp, 'x').format('hh:mm a'));
+          var checkInTime = moment(timestamp, 'x').format('hh:mm a');
+          alert(`your coordinates are ${location.coords.latitude}, ${location.coords.longitude}; and you checked in at ${checkInTime}`);
           console.log(location.coords.latitude);
           console.log(location.coords.longitude);
           var latNew = location.coords.latitude;
@@ -27,4 +19,4 @@ function getLocation() {
       })
   };
 
-weatherBtn.addEventListener('click', getLocation);
+  checkInBtn.addEventListener('click', getLocation);
