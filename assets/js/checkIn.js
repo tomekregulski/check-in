500002a52f6368e35d54513848f84ae6086204d8
Tomek <tomek.regulski@gmail.com>
2021-03-09 12:19:11 -0500

    adjusted alert text

--- assets/js/checkIn.js
+++ assets/js/checkIn.js
@@ -1,21 +1,19 @@
-var cityName = document.getElementById('cityName');
-var savedCities = document.getElementById('savedCities');
-var savedCitiesArray = JSON.parse(localStorage.getItem("savedCities")) || [];
+// commented out until needed by other functions
+// var cityName = document.getElementById('cityName');
+// var savedCities = document.getElementById('savedCities');
+// var savedCitiesArray = JSON.parse(localStorage.getItem("savedCities")) || [];
 var checkInBtn = document.getElementById('checkInBtn');
 
 
 function getLocation() {
     window.navigator.geolocation.getCurrentPosition((location) => {
-          console.log(location);
-          console.log(location.timestamp);
           var timestamp = location.timestamp;
-          console.log(moment(timestamp, 'x').format('hh:mm a'));
           var checkInTime = moment(timestamp, 'x').format('hh:mm a');
-          alert(`your coordinates are ${location.coords.latitude}, ${location.coords.longitude}; and you checked in at ${checkInTime}`);
-          console.log(location.coords.latitude);
-          console.log(location.coords.longitude);
-          var latNew = location.coords.latitude;
-          var lonNew = location.coords.longitude;
+          var checkInDate = moment(timestamp, 'x').format('MM/DD/YYYY');
+          alert(`Hello! Your coordinates are ${location.coords.latitude}, ${location.coords.longitude}; and you checked in at ${checkInTime} on ${checkInDate}.`);
+          // commented out until needed by other functions
+          // var latNew = location.coords.latitude;
+          // var lonNew = location.coords.longitude;
       })
   };
 
